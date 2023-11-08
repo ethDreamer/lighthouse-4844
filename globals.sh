5b25ad68fae37c707e4809e3c1ea30d3a754180e
ethDreamer <37123614+ethDreamer@users.noreply.github.com>
2023-11-08 15:03:41 -0600

    Work with Mac Permissions (#16)

--- globals.sh
+++ globals.sh
@@ -2,7 +2,7 @@
 
 export ETH_TESTNET=devnet-11
 
-export EXECUTION_NODE=besu # can be [geth|nethermind|besu|reth]
+export EXECUTION_NODE=nethermind # can be [geth|nethermind|besu|reth]
 export CONSENSUS_DISC=9000  # discovery TCP/UDP port open to internet for lighthouse
 export EXECUTION_DISC=30303 # discovery TCP/UDP port open to internet for execution node
 
@@ -17,7 +17,7 @@ export IMAGE_PREFIX="${ETH_TESTNET//-/}"
 export CONTAINER_PREFIX="${ETH_TESTNET//-/}"
 
 # docker images
-export LIGHTHOUSE_IMAGE=ethdreamer/lighthouse:deneb-sync-fix
+export LIGHTHOUSE_IMAGE=sigp/lighthouse:latest-unstable
 export GETH_IMAGE=ethpandaops/geth:lightclient-devnet-10-4d161de
 export BESU_IMAGE=hyperledger/besu:develop
 export NETHERMIND_IMAGE=ethpandaops/nethermind:master-4847b06
@@ -55,7 +55,7 @@ check_permissions() {
   local context=$4
 
   if [ "$user_uid" -eq "0" ]; then
-    return
+    return # root user bypasses permission checks
   fi
 
   if [ ! -d "$directory" ]; then
@@ -63,37 +63,75 @@ check_permissions() {
     return
   fi
 
-  local dir_stats=$(stat -c "%u %g %a" "$directory")
-  read -r dir_uid dir_gid dir_perms <<< "$dir_stats"
+  local dir_uid dir_gid dir_perms platform
+  platform=$(uname)
 
-  local user_perms=$(( dir_perms / 100 ))
-  local group_perms=$(( (dir_perms / 10) % 10 ))
-  local other_perms=$(( dir_perms % 10 ))
+  if [[ "$platform" == "Darwin" ]]; then
+    # Fetch user and group name and then convert to ID
+    local owner_name=$(stat -f "%Su" "$directory")
+    local group_name=$(stat -f "%Sg" "$directory")
 
-  local user_has_read=$(( user_perms / 4 ))
-  local user_has_write=$(( (user_perms / 2) % 2 ))
-  local user_has_execute=$(( user_perms % 2 ))
-
-  if [ "$user_uid" -ne "$dir_uid" ]; then
-    if [ "$user_gid" -eq "$dir_gid" ]; then
-      user_has_read=$(( group_perms / 4 ))
-      user_has_write=$(( (group_perms / 2) % 2 ))
-      user_has_execute=$(( group_perms % 2 ))
+    # Convert owner name to UID, if it's not a number already
+    if [[ "$owner_name" =~ ^[0-9]+$ ]]; then
+      dir_uid=$owner_name
     else
-      user_has_read=$(( other_perms / 4 ))
-      user_has_write=$(( (other_perms / 2) % 2 ))
-      user_has_execute=$(( other_perms % 2 ))
+      dir_uid=$(id -u "$owner_name" 2>/dev/null)
+      [[ -z $dir_uid ]] && echo "WARNING: No user ID found for owner $owner_name. Using 0 as a fallback." && dir_uid=0
     fi
+
+    # Convert group name to GID, if it's not a number already
+    if [[ "$group_name" =~ ^[0-9]+$ ]]; then
+      dir_gid=$group_name
+    else
+      dir_gid=$(dscl . -read "/Groups/$group_name" | awk '/PrimaryGroupID:/{print $2}' 2>/dev/null)
+      [[ -z $dir_gid ]] && echo "WARNING: No group ID found for group $group_name. Using 0 as a fallback." && dir_gid=0
+    fi
+
+    # Use the '%A' format for numeric access rights on macOS
+    dir_perms=$(stat -f "%A" "$directory")
+  else
+    # On Linux, use 'stat' to get IDs and permissions directly
+    read -r dir_uid dir_gid dir_perms <<< $(stat -c "%u %g %a" "$directory")
   fi
 
-  if [ "$user_has_read" -eq "0" ] || [ "$user_has_write" -eq "0" ] || [ "$user_has_execute" -eq "0" ]; then
-    missing=""
-    [ "$user_has_read" -eq "0" ] && missing="read"
-    [ "$user_has_write" -eq "0" ] && [ -n "$missing" ] && missing="$missing,write" || [ -z "$missing" ] && missing="write"
-    [ "$user_has_execute" -eq "0" ] && [ -n "$missing" ] && missing="$missing,execute" || [ -z "$missing" ] && missing="execute"
-    echo "WARNING: $context - User $user_uid:$user_gid does NOT have $missing permissions on $directory."
+  # Calculate the effective permissions considering user, group, and others
+  local effective_read=0 effective_write=0 effective_execute=0
+
+  # Parse permissions to a user-readable format, considering ownership and group membership
+  local user_bit=$(( dir_perms / 100 ))
+  local group_bit=$(( (dir_perms / 10) % 10 ))
+  local other_bit=$(( dir_perms % 10 ))
+
+  # Check for user permissions if the user is the owner
+  if [ "$user_uid" -eq "$dir_uid" ]; then
+    effective_read=$(( user_bit & 4 ))
+    effective_write=$(( user_bit & 2 ))
+    effective_execute=$(( user_bit & 1 ))
+  fi
+
+  # Check for group permissions if the user is not the owner but belongs to the group
+  if [ "$user_gid" -eq "$dir_gid" ]; then
+    effective_read=$(( effective_read | (group_bit & 4) ))
+    effective_write=$(( effective_write | (group_bit & 2) ))
+    effective_execute=$(( effective_execute | (group_bit & 1) ))
+  fi
+
+  # Lastly, apply 'other' permissions for all users
+  effective_read=$(( effective_read | (other_bit & 4) ))
+  effective_write=$(( effective_write | (other_bit & 2) ))
+  effective_execute=$(( effective_execute | (other_bit & 1) ))
+
+  # Based on effective permissions, generate warning messages
+  local missing=""
+  [ "$effective_read" -eq "0" ] && missing+="read "
+  [ "$effective_write" -eq "0" ] && missing+="write "
+  [ "$effective_execute" -eq "0" ] && missing+="execute"
+
+  if [ -n "$missing" ]; then
+    echo "WARNING: $context - User $user_uid:$user_gid does NOT have $(echo $missing | xargs) permissions on $directory."
   fi
 }
+
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
